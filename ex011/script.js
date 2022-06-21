443985bb31a3d1bbf1f45c91a5c6ffaa49a7c02e
Kohsey Dufour <kohsey.dufour@mail.com>
2022-06-21 14:51:47 +0200

    ex 14

--- ex011/script.js
+++ ex011/script.js
@@ -1,10 +1,17 @@
-let c = document.querySelector("input").value;
-console.log(c);
-
-function convert(c) {
-    return  ((9 * c + 32 * 5)/5);
+function cToF(celsius) 
+{
+  let cTemp = celsius;
+  let cToFahr = cTemp * 9 / 5 + 32;
+  let message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+    console.log(message);
 }
 
-let r = convert(c);
-
-document.querySelector(".result").textContent = r;
+function fToC(fahrenheit) 
+{
+  let fTemp = fahrenheit;
+  let fToCel = (fTemp - 32) * 5 / 9;
+  let message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+    console.log(message);
+} 
+cToF(60);
+fToC(45);
---
+++ ex013/script.js
@@ -0,0 +1,8 @@
+let my_var = "nom";
+let my_value = "Kohsey";
+
+
+let x = {
+  my_var: my_value,
+};
+console.log(x["my_var"]);
